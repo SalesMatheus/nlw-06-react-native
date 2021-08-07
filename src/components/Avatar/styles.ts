d6a317cf44f9778cf75d9d277c90844fb0575836
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-08-07 20:56:15 -0300

    Adicionando tela Home

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+
+    },
+    avatar:{
+        width: 53,
+        height: 53,
+        borderRadius: 8,
+        marginRight: 22,
+    }
+});
