413343692c7d77cf2dee6c4b8eece6e0c03fe61a
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-08-10 21:13:56 -0300

    Adicionando listagem de categorias

---
+++ src/components/CategorySelect/styles.ts
@@ -0,0 +1,7 @@
+import { StyleSheet, TextInputBase } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+    container: {
+    }
+});
---
+++ src/utils/categories.ts
@@ -0,0 +1,11 @@
+import RankedSvg from '../assets/ranked.svg';
+import DuelSvg from '../assets/duel.svg';
+import FunSvg from '../assets/fun.svg';
+import TrainingSvg from '../assets/training.svg';
+
+export const categories = [
+    {id: '1', title: 'Ranked', icon: RankedSvg},
+    {id: '2', title: 'PvP', icon: DuelSvg},
+    {id: '3', title: 'Normal Game', icon: FunSvg},
+    {id: '4', title: 'Training', icon: TrainingSvg},
+]
