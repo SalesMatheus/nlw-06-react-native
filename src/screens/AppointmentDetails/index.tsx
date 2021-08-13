f8b6aea46989a3bf5c6f4924f71e3cfbe96343ff
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-08-12 21:29:35 -0300

    Adicionando detalhamento de membros

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
-import { View } from "react-native";
+import React from "react";
+import { ImageBackground, Text, View, FlatList } from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
 import { Fontisto } from "@expo/vector-icons";
 
+import BannerImg from "../../assets/banner.png"
 import { Header } from "../../components/Header";
+import { ListDivider } from "../../components/ListDivider";
 import { Background } from "../../components/background";
-import { styles } from "./styles"
+import { ListHeader } from "../../components/ListHeader";
+import { Member } from "../../components/Member";
 import { theme } from "../../global/styles/theme";
+import { styles } from "./styles"
 
 export function AppointmentDetails(){
-
+    const members =[
+        {
+            id: '1',
+            username: 'Rodrigo',
+            avatar_url: 'https://github.com/rodrigorgtic.png',
+            status: 'online'
+        },
+        {
+            id: '2',
+            username: 'Matheus',
+            avatar_url: 'https://github.com/SalesMatheus.png',
+            status: 'offline'
+        },
+        {
+            id: '3',
+            username: 'Amanda',
+            avatar_url: 'https://github.com/alimuraamanda.png',
+            status: 'online'
+        }
+    ]
     return(
         <Background>
             <Header 
@@ -23,6 +46,33 @@ export function AppointmentDetails(){
                         />
                     </BorderlessButton>
                 }
+            />
+            <ImageBackground 
+                source={ BannerImg }
+                style={styles.banner }
+            >
+                <View style={styles.bannerContent}>
+                    <Text style={styles.title}>
+                        Lendários
+                    </Text>
+
+                    <Text style={styles.subtitle}> 
+                        É hoje que vamos chegar ao challenger sem perder uma partida da md10
+                    </Text>
+                </View>
+            </ImageBackground>
+            <ListHeader 
+                title="Jogadores"
+                subtitle="Total 3"
+            />
+            <FlatList 
+                data={members}
+                keyExtractor={ item =>item.id}
+                renderItem={({ item }) => (
+                    <Member data={ item } />         
+                )}
+                ItemSeparatorComponent={() => <ListDivider/>}
+                style={ styles.members}
             />
         </Background>
     );
